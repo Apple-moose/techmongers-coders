8b7ed72518a43e1004e25f2edc4a527201c0feeb
Apple-moose <patrissio@gmail.com>
2024-04-05 16:20:37 +0200

    1st solution KYNA 3.2.1 (no thunk yet)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import moment from "moment";
+
+const API_URL = `https://coders-network-api.onrender.com`;
 
 export default function HomePage() {
-  return <h1>Hello!</h1>;
+  const [data, setData] = useState({
+    loading: true,
+    posts: [],
+  });
+
+  async function fetchPosts() {
+    setData({ ...data, loading: true });
+
+    const response = await axios.get(`${API_URL}/posts`);
+    const posts = response.data.rows;
+
+    setData({
+      loading: false,
+      posts: posts,
+    });
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts:</h1>
+      {data.loading
+        ? "LOADING"
+        : data.posts.map((post) => (
+            <p key={post.id}>
+              <h2>{post.title}</h2>
+              <p>
+                {moment(post.updatedAt).format("DD-MM-YYYY")} *{" "}
+                {post.tags.map((p) => (
+                  <span className="greybox">{p.tag} </span>
+                ))}
+              </p>
+            </p>
+          ))}
+    </div>
+  );
 }
